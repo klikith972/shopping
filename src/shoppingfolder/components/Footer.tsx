1c6e6fd8966eb802c884e862eaf645e6cc3714af
klikith972 <klikith972@gmail.com>
2025-12-16 20:23:38 +0530

    footer customized by removing the anner

--- src/shoppingfolder/components/Footer.tsx
+++ src/shoppingfolder/components/Footer.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 
+//const [];
+
 const Footer = () => {
   return (
     <div className="footersection">
-      <div className="bannerBox">
-        <img src="assets/footer.png" alt="banner"></img>
+      <div className="Box">
+        <h3>Subscribe to our awesome emails</h3>
+        <br />
+        <p>Get our latest offers and news straiht in your inbox</p>
+        {/*<img src="assets/footer.png" alt="banner"></img>*/}
+        <input type="text" placeholder="Please enter an email adress"></input>
+        {/*<button onClick={}></button>*/}
+      </div>
+      <div className="mbox">
+        <h3>Hey,Happy shopping 🥳🥳🥳🥳</h3>
+      </div>
+      <div className="rbox">
+        <h3>Download our Apps</h3>
+        <p>shop on our products and offer on the go</p>
       </div>
     </div>
   );
